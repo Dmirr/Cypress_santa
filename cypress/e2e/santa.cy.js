381c68fa2df4ad302681254c592ac394b8eb82a6
Dmirr <dmitriyrodionow@gmail.com>
2023-04-19 23:35:10 +0200

    2

--- cypress/e2e/santa.cy.js
+++ cypress/e2e/santa.cy.js
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { faker } from "@faker-js/faker";
+describe("Santa login", () => {
+  it("user can login", () => {
+    let oldPass = "test2077";
+    let newPass = faker.internet.password(8); //8 charters
+    cy.log(newPass);
+    cy.visit("https://santa-secret.ru");
+    cy.contains("Вход и регистрация").click({ force: true });
+    cy.get(":nth-child(3) > .frm").type("testdmirr@gmail.com");
+    cy.get(":nth-child(4) > .frm").type(oldPass);
+    cy.get(".btn-main").click();
+    cy.contains("Коробки").should("exist");
+    cy.changePass(newPass);
+    cy.contains("Выйти с сайта").click();
+
+    cy.visit("https://santa-secret.ru");
+    cy.contains("Вход и регистрация").click({ force: true });
+    cy.get(":nth-child(3) > .frm").type("testdmirr@gmail.com");
+    cy.get(":nth-child(4) > .frm").type(oldPass);
+    cy.get(".btn-main").click();
+    cy.contains("Неверное имя пользователя").should("exist");
+    cy.get(":nth-child(4) > .frm").clear().type(newPass);
+    cy.get(".btn-main").click();
+    cy.changePass(oldPass);
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("changePass", (newPassword) => {
+  cy.get(
+    '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account"] > .header-item > .header-item__text > .txt--med'
+  ).click();
+  cy.get(".layout-column-start > :nth-child(1) > .frm").type(newPassword);
+  cy.get(
+    ":nth-child(4) > .form-page-group__main > .layout-column-start > :nth-child(2) > .frm"
+  ).type(newPassword);
+  cy.get(".layout-row-end > .btn-service").click();
+});
