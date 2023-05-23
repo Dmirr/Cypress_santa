084dcc7a773d8bd274835fb32640956304e9d8e6
Dmirr <dmitriyrodionow@gmail.com>
2023-05-23 18:52:33 +0200

    hooks added

--- cypress/e2e/spec_santa.cy.js
+++ cypress/e2e/spec_santa.cy.js
@@ -1,24 +1,25 @@
 beforeEach(() => {
   cy.visit("/");
+  cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
 });
 const userLoginData = require("../fixtures/pages/userLoginData.json");
 describe("Tests for Коробка", () => {
   it("passes", () => {
-    cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
+    //cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
     cy.contains("Коробки").click({ force: true });
     cy.contains("Мои Коробки").should("exist");
   });
 });
 describe("Tests for Создать коробку", () => {
   it("passes", () => {
-    cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
+    //cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
     cy.contains("Создать коробку").click();
     cy.contains("Придумайте название коробке").should("exist");
   });
 });
 describe("Tests for быстрая жеребьевка", () => {
   it("passes", () => {
-    cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
+    //cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
     cy.get('[href="/randomizer"] > .btn-secondary').click();
     cy.contains(
       "Здесь вы можете провести случайную жеребьевку между участниками"
@@ -27,7 +28,7 @@ describe("Tests for быстрая жеребьевка", () => {
 });
 describe("Tests for Личный кабинет", () => {
   it("passes", () => {
-    cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
+    //cy.userLogin1(userLoginData.userEmail, userLoginData.userPass);
     cy.contains("Дмитрий").click({ force: true });
     cy.contains("Настройки профиля").should("exist");
   });
